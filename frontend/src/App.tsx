c39f43673731618cf5ab0fd2dbc754d62af428fb
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-10-14 21:24:39 +0700

    feat(FE): add DashboardPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactElement } from 'react'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import { customTheme } from './theme/customTheme'
+import DashboardPage from './pages/dashboard'
 
 // App is a functional component and it returns a React element
 
@@ -8,7 +9,7 @@ const App: FC = (): ReactElement => {
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline /> {/* Reset the default CSS value base on the theme we passed into the ThemeProvider */}
-      <h1>Hello World</h1>
+      <DashboardPage />
     </ThemeProvider>
   )
 }
