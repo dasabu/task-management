c6c00b63fc8fcbb5037330aec8b1c3d70017f785
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-10-16 02:01:52 +0700

    chore(BE): add eslint and prettier config

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express, { type Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+
+const port = process.env.PORT || 5002
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello via Bun')
+})
+
+app.listen(port)
